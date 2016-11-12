61fa5b0e4330f75054b343262ccab34e15a33318
Diogo <diogo_miloco@hotmail.com>
2016-11-12 14:39:01 -0200

    Criação das paginas iniciais

--- app.js
+++ app.js
@@ -56,5 +56,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.listen(3000, function(){
+  console.log("Funciona!!! Porta 3000");
+})
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/mapa', function(req, res, next) {
+  res.render('mapa');
+});
+
+
 module.exports = router;
